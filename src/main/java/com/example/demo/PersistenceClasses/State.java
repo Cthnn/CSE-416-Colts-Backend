69efe65f2f6a570fbf0e3442dc54c605c527173d
Zheng Lu <zheng.lu@stonybrook.edu>
2020-11-18 13:41:04 -0500

    moved annotations to methods

--- src/main/java/com/example/demo/PersistenceClasses/State.java
+++ src/main/java/com/example/demo/PersistenceClasses/State.java
@@ -2,6 +2,7 @@ package com.example.demo.PersistenceClasses;
 
 import java.util.Map;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -14,22 +15,14 @@ import com.example.demo.EnumClasses.*;
 
 @Entity
 public class State {
-    @Id
     private int stateId;
-    @Enumerated(EnumType.STRING)
     private StateName state;
     private String districtPath;
     private String precinctPath;
     private String heatMapPath;
-
-    @OneToMany
-    @JoinColumn(name = "state_id")
-    @MapKey(name = "geoId")
     private Map<String, Precinct> precincts;
 
-    public State(){
-        // needed for JPA
-    }
+    public State(){/* needed for JPA */}
 
     public State(int stateId, StateName state, String districtPath, String precinctPath, String heatMapPath){
         this.stateId = stateId;
@@ -38,24 +31,51 @@ public class State {
         this.precinctPath = precinctPath;
         this.heatMapPath = heatMapPath;
     }
+    @Id
     public int getStateId(){
         return stateId;
     }
+    public void setStateId(int id){
+        stateId = id;
+    }
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "state")
     public StateName getStateName(){
         return state;
     }
+    public void setStateName(StateName s){
+        state = s;
+    }
+
     public String getDistrictPath(){
         return districtPath;
+    }
+    public void setDistrictPath(String path){
+        districtPath = path;
     }
     public String getHeatMapPath(){
         return heatMapPath;
     }
+    public void setHeatMapPath(String path){
+        heatMapPath = path;
+    }
     public String getPrecinctPath(){
         return precinctPath;
     }
+    public void setPrecinctPath(String path){
+        precinctPath = path;
+    }
 
+    @OneToMany
+    @JoinColumn(name = "state_id")
+    @MapKey(name = "geoId")
     public Map<String, Precinct> getPrecincts(){
         return precincts;
+    }
+
+    public void setPrecincts(Map<String, Precinct> p){
+        precincts = p;
     }
 
     public Precinct getPrecinct(String geoId){
